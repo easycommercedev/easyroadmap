0862cb6e22eef8bfa72f24acd8d2ad303f3d40a3
Micha Krapp <michakrapp@gmail.com>
2025-07-11 21:25:58 +0200

    feat(roadmap): add block with handling scripts

---
+++ app/Blocks/roadmap/render.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
+ */
+
+use EasyRoadmap\Controller\Common\Roadmap;
+
+//TODO: add product attribute
+//echo get_block_wrapper_attributes();
+?>
+
+<?php echo Roadmap::get_roadmap(); ?>
