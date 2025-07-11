b1e66a85038826066defaf8f3630b34db082832d
Micha Krapp <michakrapp@gmail.com>
2025-07-11 21:24:23 +0200

    feat(roaddmap): move to own controller

---
+++ app/Controller/Common/Roadmap.php
@@ -0,0 +1,54 @@
+<?php
+namespace EasyRoadmap\Controller\Common;
+
+defined( 'ABSPATH' ) || exit;
+
+use EasyRoadmap\Helper\Utility;
+
+class Roadmap {
+
+    public static function get_roadmap( $product = null ) {
+        $tasks  = array();
+		$stages = get_terms(
+			array(
+				'taxonomy'   => 'task_stage',
+				'hide_empty' => false,
+				'orderby'    => 'id',
+				'order'      => 'ASC',
+			)
+		);
+
+		foreach ( $stages as $stage ) {
+			$tasks[ $stage->slug ]['id']   = $stage->term_id;
+			$tasks[ $stage->slug ]['name'] = $stage->name;
+
+			$tax_query = array();
+
+			$tax_query[] = array(
+				'taxonomy' => 'task_stage',
+				'field'    => 'slug',
+				'terms'    => $stage->slug,
+			);
+
+			if ( ! is_null( $product ) ) {
+				$tax_query[] = array(
+					'taxonomy' => 'task_product',
+					'field'    => 'term_id',
+					'terms'    => $product,
+				);
+			}
+
+			$tasks[ $stage->slug ]['tasks'] = Utility::get_posts(
+				array(
+					'post_type'      => 'task',
+					'tax_query'      => $tax_query,
+					'posts_per_page' => -1,
+					'orderby'        => 'menu_order',
+					'order'          => 'ASC',
+				)
+			);
+		}
+
+		return Utility::get_template( 'shortcodes/roadmap.php', array( 'tasks' => $tasks ) );
+    }
+}
--- app/Controller/Public/Shortcode.php
+++ app/Controller/Public/Shortcode.php
@@ -3,8 +3,8 @@ namespace EasyRoadmap\Controller\Public;
 
 defined( 'ABSPATH' ) || exit;
 
+use EasyRoadmap\Controller\Common\Roadmap;
 use EasyRoadmap\Trait\Hook;
-use EasyRoadmap\Helper\Utility;
 
 class Shortcode {
 
@@ -19,48 +19,7 @@ class Shortcode {
 
 	public function callback_roadmap( $atts ) {
 		$atts = shortcode_atts( array( 'product' => null ), $atts, 'roadmap' );
-
-		$tasks  = array();
-		$stages = get_terms(
-			array(
-				'taxonomy'   => 'task_stage',
-				'hide_empty' => false,
-				'orderby'    => 'id',
-				'order'      => 'ASC',
-			)
-		);
-
-		foreach ( $stages as $stage ) {
-			$tasks[ $stage->slug ]['id']   = $stage->term_id;
-			$tasks[ $stage->slug ]['name'] = $stage->name;
-
-			$tax_query = array();
-
-			$tax_query[] = array(
-				'taxonomy' => 'task_stage',
-				'field'    => 'slug',
-				'terms'    => $stage->slug,
-			);
-
-			if ( ! is_null( $atts['product'] ) ) {
-				$tax_query[] = array(
-					'taxonomy' => 'task_product',
-					'field'    => 'term_id',
-					'terms'    => $atts['product'],
-				);
-			}
-
-			$tasks[ $stage->slug ]['tasks'] = Utility::get_posts(
-				array(
-					'post_type'      => 'task',
-					'tax_query'      => $tax_query,
-					'posts_per_page' => -1,
-					'orderby'        => 'menu_order',
-					'order'          => 'ASC',
-				)
-			);
-		}
-
-		return Utility::get_template( 'shortcodes/roadmap.php', array( 'tasks' => $tasks ) );
+		$product = $atts[ 'product' ] ?? null;
+		Roadmap::get_roadmap( $product );
 	}
 }
