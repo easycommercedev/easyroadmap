9659388faabb0392ef0eb2e856a76be7ac98a337
Micha Krapp <michakrapp@gmail.com>
2025-07-12 14:56:56 +0200

    feat(roamap): show number of tasks per stage

--- views/shortcodes/roadmap.php
+++ views/shortcodes/roadmap.php
@@ -10,13 +10,17 @@ $show_stage_links = $args['show_stage_links'] ?? false;
 <div class="er-flex er-justify-center er-space-x-4 er-mt-10 er-kanban-columns">
 	<?php foreach ( $tasks as $slug => $column ) : ?>
 		<div class="er-kanban-column" id="er-stage-<?php echo esc_attr( $column['id'] ); ?>" style="background: <?php echo esc_attr( get_term_meta( $column['id'], 'color', true ) ); ?>;">
-		<?php if( $show_stage_links ): ?>
-			<a href="<?php echo get_term_link( (int) $column['id'], 'task_stage' ); ?>">	
-				<h3 class="er-text-xl er-font-bold er-mb-4"><?php echo esc_attr( $column['name'] ); ?></h3>
-			</a>
-		<?php else: ?>
-			<h3 class="er-text-xl er-font-bold er-mb-4"><?php echo esc_attr( $column['name'] ); ?></h3>
-		<?php endif; ?>
+			
+			<h3 class="er-text-xl er-font-bold er-mb-4">
+				<?php if( $show_stage_links ): ?>
+					<a href="<?php echo get_term_link( (int) $column['id'], 'task_stage' ); ?>"><?php echo esc_attr( $column['name'] ); ?></a>
+				<?php else: ?>
+					<?php echo esc_attr( $column['name'] ); ?>
+				<?php endif; ?>
+
+				<span><?php printf( '(%d)', count( $column['tasks'] ) ); ?></span>
+			</h3>
+		
 			<?php if ( ! empty( $column['tasks'] ) ) : ?>
 				<?php foreach ( $column['tasks'] as $task_id => $task_name ) : ?>
 					<div class="er-kanban-item" id="er-task-<?php echo esc_attr( $task_id ); ?>"><?php echo esc_html( $task_name ); ?></div>
